36cb927c6a1108dd41ef300f419407f39d6635df
Mallikarjun-reddy11 <mallikarjunpandre55@gmail.com>
2026-07-27 21:51:48 +0530

    files added

---
+++ Objects/Main.java
@@ -0,0 +1,13 @@
+package Objects;
+
+public class Main {
+public static void main(String[] args) {
+	Student s=new Student(101,"Mallikarjun","AIML");
+	System.out.println("Student id: "+s.id());
+		System.out.println("Student name: "+s.name());
+		System.out.println("Studnet course: "+s.course());
+		
+		
+	
+}
+}
---
+++ Objects/Student.java
@@ -0,0 +1,6 @@
+package Objects;
+
+	public record Student(int id,String name,String course){
+	
+}
+
